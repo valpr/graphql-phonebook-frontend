2084b9fca9f1ef1ab7399bd561e4623c7e5a0c1d
Matti Luukkainen <mluukkai@iki.fi>
2020-02-21 11:55:41 +0200

    part8-6

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
-import { useQuery } from '@apollo/client'
+import { useQuery, useApolloClient } from '@apollo/client'
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
 import PhoneForm from './components/PhoneForm'
+import LoginForm from './components/LoginForm'
 import { ALL_PERSONS } from './queries'
 
 const Notify = ({ errorMessage }) => {
@@ -18,11 +19,19 @@ const Notify = ({ errorMessage }) => {
 }
 
 const App = () => {
+  const [token, setToken] = useState(null)
   const [errorMessage, setErrorMessage] = useState(null)
   const result = useQuery(ALL_PERSONS)
+  const client = useApolloClient()
 
   if (result.loading)  {
     return <div>loading...</div>
+  }
+
+  const logout = () => {
+    setToken(null)
+    localStorage.clear()
+    client.resetStore()
   }
 
   const notify = (message) => {
@@ -32,8 +41,22 @@ const App = () => {
     }, 5000)
   }
 
+  if (!token) {
+    return (
+      <div>
+        <Notify errorMessage={errorMessage} />
+        <h2>Login</h2>
+        <LoginForm
+          setToken={setToken}
+          setError={notify}
+        />
+      </div>
+    )
+  }
+
   return (
     <div>
+      <button onCLick={logout} >logout</button>
       <Notify errorMessage={errorMessage} />
       <Persons persons={result.data.allPersons} />
       <PersonForm setError={notify} />
